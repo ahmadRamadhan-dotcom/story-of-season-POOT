a95ed0d98a4b24a2a1c8cdb6e49d9d00696ed449
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-25 12:46:15 +0700

    chore: responsive overview for mobile devices

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -6,22 +6,22 @@
       class="grid place-content-center place-items-center text-center md:text-left items-center"
     >
       <div
-        class="sm:grid sm:place-content-center sm:place-items-center md:flex xl:flex xl:justify-start xl:place-items-start"
+        class="grid place-content-center place-items-center sm:grid sm:place-content-center sm:place-items-center md:flex xl:flex xl:justify-start xl:place-items-start"
       >
         <img
           src="../assets/hen.png"
           alt=""
-          class="sm:h-24 md:h-[110px] xl:h-[140px] sm:translate-y-3 md:translate-x-4 xl:translate-y-14 xl:-translate-x-1"
+          class="h-24 sm:h-24 md:h-[110px] xl:h-[140px] sm:translate-y-3 md:translate-x-4 xl:translate-y-14 xl:-translate-x-1"
         />
         <img
           src="../assets/ss_intro1_thumb.jpg"
           alt=""
-          class="sm:h-[330px] md:h-[170px] xl:h-[230px] sm:-translate-y-6 md:-translate-x-6 md:-translate-y-0 xl:-translate-x-12 border-2 shadow-md"
+          class="h-48 sm:h-[330px] md:h-[170px] xl:h-[230px] -translate-y-10 sm:-translate-y-6 md:-translate-x-6 md:-translate-y-0 xl:-translate-x-12 border-2 shadow-md"
         />
         <div
-          class="grid sm:w-[599px] md:w-[370px] sm:mt-10 md:mt-0 xl:w-[480px] md:-translate-x-1 xl:-translate-x-6 text-['Ubuntu'] text-[#673e2c]"
+          class="grid w-[340px] sm:w-[599px] md:w-[370px] sm:mt-10 md:mt-0 xl:w-[480px] md:-translate-x-1 xl:-translate-x-6 text-['Ubuntu'] text-[#673e2c]"
         >
-          <span class="sm:text-lg xl:text-[19px] font-[300]">
+          <span class="text-lg sm:text-lg xl:text-[19px] font-[300]">
             The long-awaited brand-new entry in the original farm/life
             simulation franchise, known in Japan as
             <span class="font-bold italic">Bokujo Monogatari,</span> makes its
@@ -29,14 +29,15 @@
             before to shape an untamed wilderness and customize their farm down
             to the very tile.
           </span>
-          <span class="sm:hidden font-[300] xl:mt-5 sm:text-lg xl:text-[19px]"
+          <span
+            class="hidden sm:hidden font-[300] xl:mt-5 sm:text-lg xl:text-[19px]"
             >A new cast of marriage candidates, a town that grows and thrives
             along with your farm, and a request system</span
           >
         </div>
       </div>
       <span
-        class="sm:hidden xl:w-[890px] xl:mt-3 font-['Ubuntu'] font-[300] xl:text-[19px] text-[#673e2c]"
+        class="hidden sm:hidden xl:w-[890px] xl:mt-3 font-['Ubuntu'] font-[300] xl:text-[19px] text-[#673e2c]"
       >
         highlight the new features, all while retaining the core elements of the
         STORY OF SEASONS series. Finding Earth Sprites while exploring your
@@ -45,7 +46,7 @@
         of a volcano!
       </span>
       <span
-        class="hidden sm:block md:block sm:w-[640px] xl:w-[890px] md:mt-7 xl:mt-3 font-['Ubuntu'] font-[300] sm:text-lg xl:text-[19px] text-[#673e2c]"
+        class="sm:block md:block w-[340px] sm:w-[640px] xl:w-[890px] md:mt-7 xl:mt-3 font-['Ubuntu'] font-[300] text-lg sm:text-lg xl:text-[19px] text-[#673e2c]"
       >
         A new cast of marriage candidates, a town that grows and thrives along
         with your farm, and a request system highlight the new features, all
@@ -55,12 +56,12 @@
         change, an island in the sky, or even the inside of a volcano!
       </span>
       <div
-        class="sm:w-[599px] xl:w-[890px] grid place-content-end sm:mt-4 xl:mt-3"
+        class="w-80 sm:w-[599px] xl:w-[890px] grid place-content-end mt-6 sm:mt-4 xl:mt-3"
       >
         <img
           src="../assets/cow.png"
           alt="cow"
-          class="sm:h-[120px] xl:h-[118px]"
+          class="h-32 sm:h-[120px] xl:h-[118px]"
         />
       </div>
     </div>

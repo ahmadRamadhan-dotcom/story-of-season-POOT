445b77365e4c63517371ac304eede96a8a7744f8
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-10-25 19:12:05 +0700

    fix: fix available now not responsive for mobile devices

--- src/components/AvailableNow.vue
+++ src/components/AvailableNow.vue
@@ -3,33 +3,33 @@
     <div class="grid place-content-center place-items-center">
       <img
         src="../assets/availablenow.png"
-        class="sm:h-[16vw] md:h-[13vw] lg:h-[12vw] xl:h-[9vw] sm:-translate-y-14"
+        class="h-[100px] sm:h-[16vw] md:h-[13vw] lg:h-[12vw] xl:h-[9vw] -translate-y-14 sm:-translate-y-14"
         alt="available board"
       />
     </div>
     <div
-      class="sm:grid sm:place-content-center sm:place-items-center lg:flex lg:items-center lg:gap-5 xl:gap-8"
+      class="grid place-content-center place-items-center sm:grid sm:place-content-center sm:place-items-center lg:flex lg:items-center lg:gap-5 xl:gap-8"
     >
       <img
         src="../assets/tree2.png"
-        class="sm:hidden lg:block lg:h-60 lg:-translate-y-20 lg:translate-x-16"
+        class="hidden sm:hidden lg:block lg:h-60 lg:-translate-y-20 lg:translate-x-16"
         alt=""
       />
       <h1
-        class="sm:text-[38px] lg:text-[45px] sm:leading-[50px] sm:w-10/12 md:w-8/12 lg:w-[550px] lg:-translate-y-[60px] lg:translate-x-12 sm:text-center font-[300] font-['astounder-squared-bb'] text-[#84b46d]"
+        class="text-center text-[32px] sm:text-[38px] lg:text-[45px] leading-[40px] sm:leading-[50px] w-11/12 sm:w-10/12 md:w-8/12 lg:w-[550px] lg:-translate-y-[60px] lg:translate-x-12 sm:text-center font-[300] font-['astounder-squared-bb'] text-[#84b46d]"
       >
         The long-awaited brand new entry in the STORY OF SEASONS series makes
         its debut on Nintendo Switch™!
       </h1>
       <img
         src="../assets/horse.png"
-        class="sm:h-32 sm:mt-10"
+        class="h-32 sm:h-32 mt-10 sm:mt-10"
         alt="nice horses"
       />
       <img
         src="../assets/tree1.png"
         alt=""
-        class="sm:hidden lg:block lg:-translate-y-[210px] lg:-translate-x-24 lg:h-60"
+        class="hidden sm:hidden lg:block lg:-translate-y-[210px] lg:-translate-x-24 lg:h-60"
       />
     </div>
   </div>
